bbcf95ee7ad43a7a078e85fb9ed11938f946e264
Walter Cordoba <walterflycba@gmail.com>
2022-09-07 06:02:26 -0300

    Subiendo la App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import ListaDeTareas from './componentes/ListaDeTareas';
+
+function App() {
+  return (
+    <div className='aplicacion-tareas'>
+     
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
